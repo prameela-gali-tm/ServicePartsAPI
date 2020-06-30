47db5e1e6b22bb31edc145580fb737361c5059d1
Abhilash Lingannagari (TMNA) <abhilash.lingannagari@toyota.com>
2020-06-30 12:48:16 -0500

    Initial Commit

---
+++ src/main/java/com/toyota/scs/serviceparts/ServicePartsApplication.java
@@ -0,0 +1,13 @@
+package com.toyota.scs.serviceparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyota/scs/serviceparts/ServicePartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyota.scs.serviceparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
