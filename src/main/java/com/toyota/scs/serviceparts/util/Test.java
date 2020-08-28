359455ab2d3056f66d6aa72f0850fd19d71adb85
Sreedhar Kadapa (TMNA) <sridhar.kadapa@toyota.com>
2020-08-28 18:49:13 +0530

    update and modified the case build quantity changes

--- src/main/java/com/toyota/scs/serviceparts/util/Test.java
+++ src/main/java/com/toyota/scs/serviceparts/util/Test.java
@@ -3,7 +3,6 @@ package com.toyota.scs.serviceparts.util;
 public class Test {
 
 	public static void main(String[] args) {
-		Validation validation = new Validation();
-		System.out.println(validation.parseTimestamp("2020-06-01T11:25"));
+		
 	}
 }
