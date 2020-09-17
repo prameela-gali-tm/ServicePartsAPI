3f1d51b7ab4af43c69d5996685097de095583fc8
Abhilash Lingannagari (TMNA) <abhilash.lingannagari@toyota.com>
2020-09-17 12:40:45 -0500

    target_type = "ip"

--- deploy/main.tf
+++ deploy/main.tf
@@ -178,7 +178,7 @@ resource "aws_lb_target_group" "scsserviceparts_tg" {
   port     = "8080"
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
+  target_type = "ip"
   tags = {
     ApplicationId          = var.application_id
     ApplicationName        = var.application_name
