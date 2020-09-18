b01ef31ccdd78cdb7fd7b191bdeaac8811165988
Abhilash Lingannagari (TMNA) <abhilash.lingannagari@toyota.com>
2020-09-18 09:48:22 -0500

    lb

--- deploy/main.tf
+++ deploy/main.tf
@@ -197,8 +197,57 @@ resource "aws_appautoscaling_target" "ecs_target" {
   }
 
 }
+
+resource "aws_alb_target_group" "load_balancer" {
+  name        = "${var.env}-tg-${var.app_name}"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+ 
+  health_check {
+   healthy_threshold   = "3"
+   interval            = "30"
+   protocol            = "HTTP"
+   matcher             = "200"
+   timeout             = "3"
+   path                = var.health_check_path
+   unhealthy_threshold = "2"
+  }
+}
+
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_lb.load_balancer.id
+  port              = 80
+  protocol          = "HTTP"
+ 
+  default_action {
+   type = "redirect"
+ 
+   redirect {
+     port        = 443
+     protocol    = "HTTPS"
+     status_code = "HTTP_301"
+   }
+  }
+}
+ 
+resource "aws_alb_listener" "https" {
+  load_balancer_arn = aws_lb.load_balancer.id
+  port              = 443
+  protocol          = "HTTPS"
+ 
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.alb_tls_cert_arn
+ 
+  default_action {
+    target_group_arn = aws_alb_target_group.load_balancer.id
+    type             = "forward"
+  }
+}
+
 # Port Based Load Balancer Target Group
-resource "aws_lb_target_group" "scsserviceparts_tg" {
+/* resource "aws_lb_target_group" "scsserviceparts_tg" {
   name     = "${var.env}-${var.app_name}-tgt"
   port     = "8080"
   protocol = "HTTP"
@@ -253,7 +302,7 @@ condition {
     #]
   #} 
 }  
-
+ */
 #############################
 # R53 enrty
 /* resource "aws_route53_record" "api_scsserviceparts_dns" {
