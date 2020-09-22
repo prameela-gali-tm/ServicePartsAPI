e070e0f06342264b99fa4532db50e6ec8ee40516
Abhilash Lingannagari (TMNA) <abhilash.lingannagari@toyota.com>
2020-09-22 15:50:47 -0500

    rollback

--- deploy/security_group.tf
+++ deploy/security_group.tf
@@ -69,11 +69,17 @@ resource "aws_security_group" "ecs_tasks" {
     protocol    = "TCP"
     cidr_blocks = var.rds_cidr
   }
- egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  egress {
+    protocol         = "tcp"
+    from_port        = 8080
+    to_port          = 8080
+    cidr_blocks      = var.lb_cidr
+  } 
+   egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "TCP"
+    cidr_blocks = var.rds_cidr
   }
   tags = {
     ApplicationId          = var.application_id
