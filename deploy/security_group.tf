c0ae2dfe6c712430ee9fbdd930d3ec8ff1c37a33
Abhilash Lingannagari (TMNA) <abhilash.lingannagari@toyota.com>
2020-09-22 09:26:45 -0500

    egress

--- deploy/security_group.tf
+++ deploy/security_group.tf
@@ -69,17 +69,11 @@ resource "aws_security_group" "ecs_tasks" {
     protocol    = "TCP"
     cidr_blocks = var.rds_cidr
   }
-  egress {
-    protocol         = "tcp"
-    from_port        = 8080
-    to_port          = 8080
-    cidr_blocks      = var.lb_cidr
-  } 
-   egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "TCP"
-    cidr_blocks = var.rds_cidr
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     ApplicationId          = var.application_id
