8ebe109a74e22db318f73ddda81997a214a0ba3b
Abhilash Lingannagari (TMNA) <abhilash.lingannagari@toyota.com>
2020-10-07 13:23:01 -0500

    po line kafka

---
+++ src/main/java/com/toyota/scs/serviceparts/model/PolineModel.java
@@ -0,0 +1,63 @@
+package com.toyota.scs.serviceparts.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PolineModel {
+
+	    @JsonProperty("EDI_PRTNR_ID") 
+	    public String eDI_PRTNR_ID;
+	    @JsonProperty("SCS_IND") 
+	    public String sCS_IND;
+	    @JsonProperty("VDR_CD") 
+	    public String vDR_CD;
+	    @JsonProperty("SUPPLR_CD") 
+	    public String sUPPLR_CD;
+	    @JsonProperty("PO_NUM") 
+	    public String pO_NUM;
+	    @JsonProperty("ORD_TYP") 
+	    public String oRD_TYP;
+	    @JsonProperty("TRANSP_CD") 
+	    public String tRANSP_CD;
+	    @JsonProperty("LINE_ITEM_NUM") 
+	    public String lINE_ITEM_NUM;
+	    @JsonProperty("PART_NUM") 
+	    public String pART_NUM;
+	    @JsonProperty("TOYOTA_PART_NUM") 
+	    public String tOYOTA_PART_NUM;
+	    @JsonProperty("ORD_QTY_PER_DDD") 
+	    public String oRD_QTY_PER_DDD;
+	    @JsonProperty("DDD") 
+	    public String dDD;
+	    @JsonProperty("HP") 
+	    public String hP;
+	    @JsonProperty("BO_QTY") 
+	    public String bO_QTY;
+	    @JsonProperty("PART_DESC") 
+	    public String pART_DESC;
+	    @JsonProperty("ANALYST_NAME") 
+	    public String aNALYST_NAME;
+	    @JsonProperty("DISTFD") 
+	    public String dISTFD;
+	    @JsonProperty("FINAL_DST") 
+	    public String fINAL_DST;
+	    @JsonProperty("DLR_ORD_REF_NUM") 
+	    public String dLR_ORD_REF_NUM;
+	    @JsonProperty("DLR_CODE") 
+	    public String dLR_CODE;
+	    @JsonProperty("DIRECT_SHP_FLG") 
+	    public String dIRECT_SHP_FLG;
+	    @JsonProperty("VDR_FILL_LT") 
+	    public String vDR_FILL_LT;
+	    @JsonProperty("VDR_XSIT_LT") 
+	    public String vDR_XSIT_LT;
+	    @JsonProperty("EDA") 
+	    public String eDA;
+	    @JsonProperty("TRACKING_FLG") 
+	    public String tRACKING_FLG;
+	    @JsonProperty("TIME_STAMP") 
+	    public String tIME_STAMP;
+	
+	    
+
+
+}
