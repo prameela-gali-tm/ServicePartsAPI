110c9c6ce09f6aec681c5ce5f84b78e3a4e96aa5
Abhilash Lingannagari (TMNA) <abhilash.lingannagari@toyota.com>
2020-10-30 11:25:17 -0500

    kafka message

--- src/main/java/com/toyota/scs/serviceparts/serviceImpl/KafkaService.java
+++ src/main/java/com/toyota/scs/serviceparts/serviceImpl/KafkaService.java
@@ -1,18 +1,24 @@
 
   package com.toyota.scs.serviceparts.serviceImpl;
   
-  import org.springframework.kafka.annotation.KafkaListener; import
+  import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import
   org.springframework.stereotype.Service;
   
   @Service 
   public class KafkaService {
-  
+	  Logger logger = LoggerFactory.getLogger(KafkaService.class);
   @KafkaListener(topics =
 // "TEST.SCS.ASN.NAPO.TOPIC",containerFactory="kafkaListner") 
   "TEST.NAPO.PURCHASEORDER.SCS.TOPIC",containerFactory="kafkaListner") 
   public void publish(String message) {
 	  System.out.println(
-  "You have a new message: " + message); }
+  "You have a new message: " + message); 
+	  logger.error("You have a new message: " + message);
+	  
+  }
   
   }
  
