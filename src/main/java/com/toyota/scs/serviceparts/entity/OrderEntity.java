ac9610ff4ceeca4b1a436c6daf16f015956323b9
Your Name <you@example.com>
2020-12-15 12:22:31 -0600

    SCS_2107 orderpriority sving

--- src/main/java/com/toyota/scs/serviceparts/entity/OrderEntity.java
+++ src/main/java/com/toyota/scs/serviceparts/entity/OrderEntity.java
@@ -27,7 +27,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="SP_ORDER")
+@Table(name = "SP_ORDER")
 public class OrderEntity implements Serializable {
 
 	/**
@@ -35,38 +35,40 @@ public class OrderEntity implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@Id	
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="orderSeqGen")
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orderSeqGen")
 	@SequenceGenerator(name = "orderSeqGen", sequenceName = "SPADM.SP_ORDER_SEQ", initialValue = 1, allocationSize = 10)
-	@Column(name="ORDER_ID")
+	@Column(name = "ORDER_ID")
 	private long id;
-	
-	@Column(name="PO_NUMBER")
+
+	@Column(name = "PO_NUMBER")
 	private String poNumber;
-	
-	@Column(name="ORDER_TYPE")
+
+	@Column(name = "ORDER_TYPE")
 	private String orderType;
-	
-	@Column(name="FINAL_DESTINATION")
+
+	@Column(name = "FINAL_DESTINATION")
 	private String finalDestination;
-	@Column(name="DEALER_CODE")
+	@Column(name = "DEALER_CODE")
 	private String dealerCode;
-	@Column(name="trans_code")
+	@Column(name = "trans_code")
 	private String transCode;
-	@Column(name="dealer_order")
+	@Column(name = "dealer_order")
 	private String dealerOrder;
-	
-	@Column(name="DIRECT_SHIP_FLAG")
+
+	@Column(name = "DIRECT_SHIP_FLAG")
 	private Boolean directShipFlag;
-	
-	
-	@Column(name="MODIFIED_BY")
+
+	@Column(name = "order_priority")
+	private String orderPriority;
+
+	@Column(name = "MODIFIED_BY")
 	private String modifiedBy;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name="MODIFIED_DATE")
+	@Column(name = "MODIFIED_DATE")
 	private Date modifiedDate;
-	
+
 	/*
 	 * @OneToOne(fetch = FetchType.LAZY, optional = false)
 	 * 
@@ -74,28 +76,20 @@ public class OrderEntity implements Serializable {
 	 * 
 	 * @JsonIgnore private Vendor vendor;
 	 */
-	@Column(name="VENDOR_CODE")
+	@Column(name = "VENDOR_CODE")
 	private String vendorCode;
 
 	@Column(name = "FINAL_DESTINATION_DESC")
 	private String finalDestDesc;
-	
+
 	public OrderEntity() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	
-
-
-	
-
-
-
-
 	public OrderEntity(long id, String poNumber, String orderType, String finalDestination, String dealerCode,
 			String transCode, String dealerOrder, Boolean directShipFlag, String modifiedBy, Date modifiedDate,
-			String vendorCode) {
+			String vendorCode,String orderPriority) {
 		super();
 		this.id = id;
 		this.poNumber = poNumber;
@@ -108,81 +102,48 @@ public class OrderEntity implements Serializable {
 		this.modifiedBy = modifiedBy;
 		this.modifiedDate = modifiedDate;
 		this.vendorCode = vendorCode;
+		this.orderPriority=orderPriority;
 	}
-
-
-
-
-
-
-
-
 
 	public String getDealerOrder() {
 		return dealerOrder;
 	}
 
-
-
-
-
-
-
-
-
 	public void setDealerOrder(String dealerOrder) {
 		this.dealerOrder = dealerOrder;
 	}
-
-
-
-
-
-
-
-
 
 	public String getFinalDestination() {
 		return finalDestination;
 	}
 
-
-
-
 	public void setFinalDestination(String finalDestination) {
 		this.finalDestination = finalDestination;
 	}
-
-
-
 
 	public String getDealerCode() {
 		return dealerCode;
 	}
 
+	public String getOrderPriority() {
+		return orderPriority;
+	}
 
-
+	public void setOrderPriority(String orderPriority) {
+		this.orderPriority = orderPriority;
+	}
 
 	public void setDealerCode(String dealerCode) {
 		this.dealerCode = dealerCode;
 	}
 
-
-
-
 	public Boolean getDirectShipFlag() {
 		return directShipFlag;
 	}
 
-
-
-
 	public void setDirectShipFlag(Boolean directShipFlag) {
 		this.directShipFlag = directShipFlag;
 	}
-
-
-
 
 	public String getPoNumber() {
 		return poNumber;
@@ -232,45 +193,20 @@ public class OrderEntity implements Serializable {
 		this.id = id;
 	}
 
-
-
-
 	public String getTransCode() {
 		return transCode;
 	}
-
-
-
 
 	public void setTransCode(String transCode) {
 		this.transCode = transCode;
 	}
 
-
-
-
-
-
-
-
-
 	public String getFinalDestDesc() {
 		return finalDestDesc;
 	}
-
-
-
-
-
-
-
-
 
 	public void setFinalDestDesc(String finalDestDesc) {
 		this.finalDestDesc = finalDestDesc;
 	}
 
-
-	
-	
 }
--- src/main/java/com/toyota/scs/serviceparts/serviceImpl/TpnaServiceImpl.java
+++ src/main/java/com/toyota/scs/serviceparts/serviceImpl/TpnaServiceImpl.java
@@ -19,104 +19,122 @@ import com.toyota.scs.serviceparts.repository.VendorRepositroy;
 import com.toyota.scs.serviceparts.service.TpnaService;
 import com.toyota.scs.serviceparts.util.CommonValidation;
 import com.toyota.scs.serviceparts.util.DateUtils;
+
 @Service
-public class TpnaServiceImpl implements TpnaService{
-	
+public class TpnaServiceImpl implements TpnaService {
+
 	@Autowired
 	OrderRepository ordRepo;
 	@Autowired
-	PartRepository partRepo;	
+	PartRepository partRepo;
 	@Autowired
 	VendorRepositroy vendorRepo;
+
 	@Override
 	public boolean poDetails(List<PolineModel> polineList) {
 
-		//---------- sp_order
-		//vDR_CD vendor code
-		//pO_NUM PO number
-		//oRD_TYP Order type
-		//tRANSP_CD trans code		
-		//DISTFD final_destination
-		//dIRECT_SHP_FLG direct_ship_flag
-		//dLR_CODE dealer_code
-		Specification<OrderEntity> ordSpec =null;
-		boolean issuesExists=false,orderExists=false,partExists=false;
-		String poNumber, orderType,vendorCode,partNumber="",poLine="",org_ddd="",eda;
-		Date dddOrgDate,edaDate;
-		Map<String,String> vendorCodeMap = new HashedMap<String, String>();
-		for(PolineModel poline :polineList) {
-			orderExists=false;
-			partExists=false;
-			poNumber="";orderType="";vendorCode="";partNumber="";poLine="";org_ddd="";eda="";
-			poNumber=poline.getpO_NUM();
-			orderType=poline.getoRD_TYP();
-			vendorCode=poline.getvDR_CD();
-			partNumber=poline.getpART_NUM();
-			poLine=poline.getlINE_ITEM_NUM();
-			org_ddd=poline.getoRIG_DDD();
-			eda=poline.geteDA();
-			dddOrgDate= DateUtils.convertfromStringToDateFmt(org_ddd, "MM/dd/yyyy");
-			edaDate= DateUtils.convertfromStringToDateFmt(eda, "MM/dd/yyyy");
-			
-			//check for required fields
-			if(CommonValidation.isNullOrEmpty(poNumber)
-			   ||CommonValidation.isNullOrEmpty(orderType)
-			   ||CommonValidation.isNullOrEmpty(vendorCode)
-					) {
-				issuesExists=true;
+		// ---------- sp_order
+		// vDR_CD vendor code
+		// pO_NUM PO number
+		// oRD_TYP Order type
+		// tRANSP_CD trans code
+		// DISTFD final_destination
+		// dIRECT_SHP_FLG direct_ship_flag
+		// dLR_CODE dealer_code
+		Specification<OrderEntity> ordSpec = null;
+		boolean issuesExists = false, orderExists = false, partExists = false;
+		String poNumber, orderType, vendorCode, partNumber = "", poLine = "", org_ddd = "", eda;
+		Date dddOrgDate, edaDate;
+		Map<String, String> vendorCodeMap = new HashedMap<String, String>();
+		for (PolineModel poline : polineList) {
+			orderExists = false;
+			partExists = false;
+			poNumber = "";
+			orderType = "";
+			vendorCode = "";
+			partNumber = "";
+			poLine = "";
+			org_ddd = "";
+			eda = "";
+			poNumber = poline.getpO_NUM();
+			orderType = poline.getoRD_TYP();
+			vendorCode = poline.getvDR_CD();
+			partNumber = poline.getpART_NUM();
+			poLine = poline.getlINE_ITEM_NUM();
+			org_ddd = poline.getoRIG_DDD();
+			eda = poline.geteDA();
+			dddOrgDate = DateUtils.convertfromStringToDateFmt(org_ddd, "MM/dd/yyyy");
+			edaDate = DateUtils.convertfromStringToDateFmt(eda, "MM/dd/yyyy");
+
+			// check for required fields
+			if (CommonValidation.isNullOrEmpty(poNumber) || CommonValidation.isNullOrEmpty(orderType)
+					|| CommonValidation.isNullOrEmpty(vendorCode)) {
+				issuesExists = true;
 				continue;
 			}
-			OrderEntity currentOrd=ordRepo.findByPoNumberAndVendorCodeAndOrderType(poNumber, vendorCode, orderType);
+			OrderEntity currentOrd = ordRepo.findByPoNumberAndVendorCodeAndOrderType(poNumber, vendorCode, orderType);
 			// insert into order table if not exists
-			if(currentOrd==null||currentOrd.getId()==0L) {
-				orderExists=false;
-				 currentOrd= new OrderEntity();
-				 
-				 currentOrd.setPoNumber(poNumber);
-				 currentOrd.setVendorCode(vendorCode);
-				 currentOrd.setOrderType(orderType);
-				 if(poline.getdIRECT_SHP_FLG().equalsIgnoreCase("Y")) {
-					 currentOrd.setDirectShipFlag(true);
-				 }else {
-					 currentOrd.setDirectShipFlag(false);
-				 }
-				 
-				 currentOrd.setFinalDestination(poline.getdISTFD());
-				 currentOrd.setFinalDestDesc(poline.getfINAL_DST());
-				 currentOrd.setTransCode(poline.gettRANSP_CD());
-				 currentOrd.setDealerCode(poline.getdLR_CODE());
-				 currentOrd.setDealerOrder(poline.getdLR_ORD_REF_NUM());
-				 currentOrd.setModifiedDate(new Date());
-				 currentOrd.setModifiedBy("TPNA");
-				 ordRepo.save(currentOrd);
-			}else {
-				orderExists=true;
+			if (currentOrd == null || currentOrd.getId() == 0L) {
+				orderExists = false;
+				currentOrd = new OrderEntity();
+
+				currentOrd.setPoNumber(poNumber);
+				currentOrd.setVendorCode(vendorCode);
+				currentOrd.setOrderType(orderType);
+				if (poline.getdIRECT_SHP_FLG().equalsIgnoreCase("Y")) {
+					currentOrd.setDirectShipFlag(true);
+				} else {
+					currentOrd.setDirectShipFlag(false);
+				}
+
+				currentOrd.setFinalDestination(poline.getdISTFD());
+				currentOrd.setFinalDestDesc(poline.getfINAL_DST());
+				currentOrd.setTransCode(poline.gettRANSP_CD());
+				currentOrd.setDealerCode(poline.getdLR_CODE());
+				currentOrd.setDealerOrder(poline.getdLR_ORD_REF_NUM());
+				currentOrd.setModifiedDate(new Date());
+				currentOrd.setModifiedBy("TPNA");
+
+				if (orderType!=null && orderType.equalsIgnoreCase("C")) {
+					currentOrd.setOrderPriority("1");
+				} else if (orderType!=null && orderType.equalsIgnoreCase("E")) {
+					currentOrd.setOrderPriority("2");
+				} else if (orderType!=null && orderType.equalsIgnoreCase("D")) {
+					currentOrd.setOrderPriority("3");
+				} else if (orderType!=null && orderType.equalsIgnoreCase("V")) {
+					currentOrd.setOrderPriority("4");
+				} else {
+					currentOrd.setOrderPriority("5");
+				}
+
+				ordRepo.save(currentOrd);
+			} else {
+				orderExists = true;
 			}
-			
+
 			PartEntity part;
-			if(orderExists) {
-				 part=	partRepo.findByOrderIdAndPartNumberAndLineItemNumberAndDeliveryDueDate
-							(currentOrd.getId(), partNumber,poLine, dddOrgDate);
-				 if(part==null||part.getId()==0L) {
-					 partExists=false;
-				 }else {
-					 partExists=true;
-				 }
+			if (orderExists) {
+				part = partRepo.findByOrderIdAndPartNumberAndLineItemNumberAndDeliveryDueDate(currentOrd.getId(),
+						partNumber, poLine, dddOrgDate);
+				if (part == null || part.getId() == 0L) {
+					partExists = false;
+				} else {
+					partExists = true;
+				}
+			} else {
+				// insert into part table
+				partExists = false;
 			}
-			else {
-				// insert into part table 
-				partExists=false;				
-			}
-		   
-			if(!partExists) {
-				 //-------------- sp_part
-				//lINE_ITEM_NUM line_item_number
-				//pART_NUM part_number
-				//dDD ddd
-				//tOYOTA_PART_NUM vendor_part_number
-				//oRD_QTY_PER_DDD order_quantity
+
+			if (!partExists) {
+				// -------------- sp_part
+				// lINE_ITEM_NUM line_item_number
+				// pART_NUM part_number
+				// dDD ddd
+				// tOYOTA_PART_NUM vendor_part_number
+				// oRD_QTY_PER_DDD order_quantity
 				//
-				part=new PartEntity();
+				part = new PartEntity();
 				part.setOrderId(currentOrd.getId());
 				part.setLineItemNumber(poLine);
 				part.setDeliveryDueDate(dddOrgDate);
@@ -133,10 +151,11 @@ public class TpnaServiceImpl implements TpnaService{
 				part.setStatus("INSERT");
 				partRepo.save(part);
 			}
-			if(!vendorCodeMap.containsKey(poline.getvDR_CD())) {
-				VendorEntity vendorEntity;				
-				vendorEntity = vendorRepo.findByVendorCodeAndTradingPartnerId(poline.getvDR_CD(), poline.geteDI_PRTNR_ID());
-				if(vendorEntity==null || vendorEntity.getId()==0L ) {
+			if (!vendorCodeMap.containsKey(poline.getvDR_CD())) {
+				VendorEntity vendorEntity;
+				vendorEntity = vendorRepo.findByVendorCodeAndTradingPartnerId(poline.getvDR_CD(),
+						poline.geteDI_PRTNR_ID());
+				if (vendorEntity == null || vendorEntity.getId() == 0L) {
 					vendorEntity = new VendorEntity();
 					vendorEntity.setVendorCode(poline.getvDR_CD());
 					vendorEntity.setTradingPartnerId(poline.geteDI_PRTNR_ID());
@@ -147,10 +166,9 @@ public class TpnaServiceImpl implements TpnaService{
 				}
 				vendorCodeMap.put(poline.getvDR_CD(), poline.getvDR_CD());
 			}
-				
-			
+
 		}
-		
+
 		return false;
 	}
 
